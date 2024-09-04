a5a63b98b41668155c8606145ea2948a6638a7e9
Mathewaudelio <134225902+Mathewaudelio@users.noreply.github.com>
2024-09-04 08:36:54 +0300

    Add files via upload

---
+++ Staff.java
@@ -0,0 +1,11 @@
+public class Staff {
+    // static variable
+    static String fName = "Obur";
+    String lName = "Audelio";
+    String email = "Obur@gmail.com";
+    int age = 71;
+    public void getdisplay() {
+        System.out.println("The staff detail  is "+ " " + fName + " " + lName + " " + email );
+    }
+
+}
